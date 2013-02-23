5a45190fca5553f65ea9ae43623ee62fd57a2f8f
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2013-02-23 13:35:38 -0300

    Added sitemap file

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -2,3 +2,4 @@ bundle install
 juicer install jslint
 juicer merge css/base.css css/pygments.css css/normalize.css -o css/app.min.css --force --document-root .
 juicer merge css/iphone.css -o css/iphone.min.css --force --document-root .
+cp _site/sitemap.xml .
