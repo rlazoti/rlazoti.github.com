a0e01e8657c87f1326441a944005591621780fe9
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2013-02-23 13:31:13 -0300

    All css files minified

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -1,6 +1,4 @@
 bundle install
 juicer install jslint
-juicer merge css/base.css      -o css/base.min.css      --force --document-root .
-juicer merge css/pygments.css  -o css/pygments.min.css  --force --document-root .
-juicer merge css/normalize.css -o css/normalize.min.css --force --document-root .
-juicer merge css/main.css      -o css/main.min.css      --force --document-root .
+juicer merge css/base.css css/pygments.css css/normalize.css -o css/app.min.css --force --document-root .
+juicer merge css/iphone.css -o css/iphone.min.css --force --document-root .
