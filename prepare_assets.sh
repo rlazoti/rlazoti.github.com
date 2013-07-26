f1c14719899b03214f11c6526fff29ac6be28b28
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2013-07-26 14:53:30 -0300

    Removed unused css files

--- prepare_assets.sh
+++ prepare_assets.sh
@@ -1,5 +1,4 @@
 bundle install
 juicer install jslint
-juicer merge css/base.css css/pygments.css css/normalize.css -o css/app.min.css --force --document-root .
-juicer merge css/iphone.css -o css/iphone.min.css --force --document-root .
+juicer merge assets/css/site.css -o assets/css/site.min.css --force --document-root .
 cp _site/sitemap.xml .
